52cf469268bb521d996d266e6b7acbd11cbcfaa7
Kevin Ngo <ngoke@mozilla.com>
2014-07-31 03:35:05 -0700

    get rid of bootstrap code

--- www/js/app.js
+++ www/js/app.js
@@ -1,28 +1,29 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+var angular = require('angular');
+require('angular-ui-router');
+require('./controllers');
+require('./services');
+require('./settings');
+
+angular.module('sidekick', ['ionic', 'sidekick.controllers',
+                            'sidekick.services'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default.
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            // org.apache.cordova.statusbar required.
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+  // AngularUI Router.
   $stateProvider
-
-    // setup an abstract state for the tabs directive
+    // Set up an abstract state for the tabs directive.
     .state('tab', {
       url: "/tab",
       abstract: true,
@@ -30,48 +31,36 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     })
 
     // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-      url: '/dash',
+    .state('tab.tracker', {
+      url: '/tracker',
       views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
+        'tracker': {
+          templateUrl: 'templates/tracker.html',
+          controller: 'TrackerCtrl'
         }
       }
     })
 
-    .state('tab.friends', {
-      url: '/friends',
+    .state('tab.tools', {
+      url: '/tools',
       views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
+        'tools': {
+          templateUrl: 'templates/tools.html',
+          controller: 'ToolsCtrl'
         }
       }
     })
 
-    .state('tab.account', {
-      url: '/account',
+    .state('tab.settings', {
+      url: '/settings',
       views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
+        'settings': {
+          templateUrl: 'templates/settings.html',
+          controller: 'SettingsCtrl'
         }
       }
     })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
+  $urlRouterProvider.otherwise('/tab/tracker');
 });
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,10 @@
-angular.module('starter.controllers', [])
+angular.module('sidekick.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('TrackerCtrl', function($scope) {
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('ToolsCtrl', function($scope) {
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
+.controller('SettingsCtrl', function($scope) {
 });
