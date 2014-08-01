6bfe1bff03d90ee984bb68120511fd0098f4d76c
Kevin Ngo <ngoke@mozilla.com>
2014-08-01 02:45:06 -0700

    reuse the poker chart i made on my blog

--- www/js/chart.js
+++ www/js/chart.js
@@ -2,55 +2,29 @@ var d3 = require('d3');
 var $ = require('jquery');
 var _ = require('underscore');
 
+
 function CumulativeLineChart(element, rawData, _cfg) {
-    var chart;
-    var chartGroup;
     var data;
+    var height;
+    var margin = {top: 10, right: 10, bottom: 40, left: 30};
+    var width;
 
     var cfg = {
-        chart: {
-            axisPadding: 3,
-            fontColor: 'rgb(245, 245, 245)',
-            fontSize: '12',
-            height: $('.chart').height(),
-            opacity: '.95',
-            topPadding: 15,
-            title: '$',
-            width: $('.chart').width()
-        },
-        line: {
-            color: '#299283'
-        },
         xAxis: {
-            days: 7,
-            padding: 5
-        },
-        yAxis: {
-            padding: 35
-        },
+            days: -1
+        }
     };
 
     init(rawData, _cfg);  // Link. Start!.
 
     function init(rawData, _cfg) {
         cfg = $.extend(true, {}, cfg, _cfg);
+
+        height = $('.chart').height() - margin.top - margin.bottom;
+        width = $('.chart').width() - margin.left - margin.right;
+
         data = transform(rawData);
         render(element);
-        console.log(data);
-    }
-
-    function transform(rawData) {
-        if (cfg.xAxis.days !== -1) {
-            rawData = trim(rawData);
-        }
-
-        // This will be our data point.
-        var cumulativeProfit = 0;
-        return _.map(rawData, function(d) {
-            cumulativeProfit += (d.result - d.buyin);
-            d.cumulativeProfit = cumulativeProfit;
-            return d;
-        });
     }
 
     function trim(rawData) {
@@ -62,88 +36,93 @@ function CumulativeLineChart(element, rawData, _cfg) {
         });
     }
 
+    function transform(rawData) {
+        // Trim to date.
+        if (cfg.xAxis.days !== -1) {
+            rawData = trim(rawData);
+        }
+
+        // Calculate profits.
+        var cumulativeProfit = 0;
+        rawData = _.map(rawData, function(d) {
+            cumulativeProfit += (d.result - d.buyin);
+            return cumulativeProfit;
+        });
+
+        // Start at 0.
+        rawData.unshift(0);
+
+        return rawData;
+    }
+
     function render(element) {
-        // Create canvas.
-        chart = d3.select(element)
-            .append('svg:svg')
-            .attr('width', cfg.chart.width)
-            .attr('height', cfg.chart.height);
+        var min = _.min(data);
+        min -= min * 0.05;
+        var max = _.max(data);
+        max += max * 0.05;
 
-        // Create scales.
-        var xScale = d3.scale.linear()
-            .domain([0, data.length])
-            .range([0, cfg.chart.width - cfg.yAxis.padding]);
-
-        var min = _.min(data, function(d) {return d.cumulativeProfit;}).cumulativeProfit;
-        var max = _.max(data, function(d) {return d.cumulativeProfit;}).cumulativeProfit;
-        var yScale = d3.scale.linear()
+        var x = d3.scale.linear()
+            .domain([0, data.length - 1])
+            .range([0, width]);
+        var y = d3.scale.linear()
             .domain([min, max])
-            .range([cfg.chart.topPadding,
-                    cfg.chart.height - cfg.xAxis.padding - cfg.chart.topPadding]);
+            .range([height, 0]);
 
-        // Group elements in the chart together to help padding.
-        chartGroup = chart.append('g')
+        var xAxis = d3.svg.axis()
+            .tickFormat(function(n) {
+                return Math.floor(n) !== n ? null : n;
+            })
+            .scale(x)
+            .orient('bottom');
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient('left');
+
+        var line = d3.svg.line()
+            .x(function(d, i) { return x(i); })
+            .y(function(d, i) { return y(d); });
+
+        var svg = d3.select(element)
+            .append('svg:svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
             .attr('transform',
-                  'translate(' + cfg.yAxis.padding + ', ' + 0 + ')');
+                  'translate(' + margin.left + ',' + margin.top + ')');
 
-        var spacing = (cfg.chart.width - cfg.yAxis.padding) / data.length;
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(xAxis)
+          .append('text')
+            .attr('x', 113)
+            .attr('dy', 35)
+            .style('text-anchor', 'end')
+            .text('Tourney/Session No.');
 
-        // X axis text.
-        chartGroup.selectAll('text.xAxis')
-            .data(data)
-            .enter()
-            .append('svg:text')
-            .attr('class', 'xAxis')
-            .attr('fill', cfg.chart.fontColor)
-            .attr('font-size', cfg.chart.fontSize + 'px')
-            .attr('text-anchor', 'middle')
-            .attr('x', function(d, i) {
-                return xScale(data.indexOf(d)) + spacing / 2;
-            })
-            .attr('y', cfg.chart.height - cfg.chart.axisPadding)
-            .text(function(d, i) {
-                var modDays = cfg.xAxis.days;
-                if (cfg.xAxis.days === -1) {
-                    // Case where we show every day.
-                    modDays = data.length;
-                }
-                if (data.length <= 7 ||
-                    data.indexOf(d) % Math.floor(modDays / 6) === 0) {
-                    // Show only about every 7 dates on xAxis.
-                    return d.date.getMonth() + 1 + '-' + d.date.getDate();
-                }
-            });
+        svg.append('g')
+            .attr('class', 'y axis')
+            .call(yAxis)
+          .append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', 6)
+            .attr('dy', '.71em')
+            .style('text-anchor', 'end')
+            .text('Profit ($)');
 
-        // Y axis text.
-        chart.selectAll('text.yAxis')
-            .data(_.range(min, max, 10))
-            .enter()
-            .append('svg:text')
-            .attr('class', 'yAxis')
-            .attr('fill', cfg.chart.fontColor)
-            .attr('font-size', cfg.chart.fontSize + 'px')
-            .attr('x', cfg.chart.axisPadding)
-            .attr('y', function(d) {
-                return yScale(d) + cfg.chart.fontSize / 2 - 1;
-            })
-            .text(function(d, i) {
-                return '$' + d;
-            });
+        svg.append('path')
+            .datum(data)
+            .attr('class', 'line')
+            .attr('d', line);
     }
 
-    function setDays(days) {
-        cfg.xAxis.days = days;
-        refresh();
-    }
-
-    function refresh(rawData) {
+    function refresh(newRawData, cfg) {
         $(element + ' svg').remove();
-        cumulativeLineChart(element, rawData);
+        init(element, newRawData, cfg);
     }
 
     return {
         refresh: refresh,
-        setDays: setDays
     };
 }
 
